c84db584d3c3823fc18b04d783193c592a666925
BrunoSouzza <souza_0010@hotmail.com>
2021-09-14 16:04:14 -0300

    configuring vuex sotcks and portfolio

--- src/components/stock/Stocks.vue
+++ src/components/stock/Stocks.vue
@@ -9,14 +9,7 @@ import Stock from './Stock.vue';
 export default {
   components: { Stock },
   data() {
-    return {
-      stocks: [
-        { id: 1, name: 'BMW', price: 100 },
-        { id: 2, name: 'Google', price: 100 },
-        { id: 3, name: 'Apple', price: 200 },
-        { id: 4, name: 'Twitter', price: 300 },
-      ],
-    };
+    return {};
   },
 };
 </script>
